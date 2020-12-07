7acd53f6326da9bdbc436389e2f37e0d795646b2
jhojun7 <jhojun@gmail.com>
2020-12-07 13:25:13 +0900

    Java / 3회차 수업 01

---
+++ Study/study1205_03lotto.java
@@ -0,0 +1,40 @@
+package my.test.study;
+
+import java.util.Random;
+
+public class study1205_lotto {
+
+	public static void main(String[] args) {
+		int[] lottoNumberArr = new int[5];
+
+		for (int i = 0; i < lottoNumberArr.length; i++) {
+
+			int number = getLottoNumber();
+			boolean isDup = false;
+			for (int j = 0; j < lottoNumberArr.length; j++) {
+				int j2 = lottoNumberArr[j];
+
+				if(j2 == number) {
+					isDup= true;
+				}
+			}
+
+
+			lottoNumberArr[i] = number;
+
+			if(isDup) {
+				i--;
+			}
+		}
+
+		////여기다 부족한거 올리기
+
+
+	}
+
+	private static int getLottoNumber() {
+		Random r = new Random();
+		int number = r.nextInt(45); // 0~44까지의 수를 랜덤으로 int 로 뽑아냄
+		return number;
+	}
+}
