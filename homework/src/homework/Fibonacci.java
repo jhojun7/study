8c7616e7613f0eacc02dd7d57f5ae43807a27b1c
jhojun7 <jhojun@gmail.com>
2021-01-12 23:22:38 +0900

    what

--- homework/src/homework/Fibonacci.java
+++ homework/src/homework/Fibonacci.java
@@ -1,5 +1,8 @@
 package homework;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;					 // Scanner 임포트
 
 public class Fibonacci {
@@ -8,23 +11,25 @@ public class Fibonacci {
 		Scanner scan = new Scanner(System.in); // scanner 기능 생성
 		int N = scan.nextInt();				// 정수 N 입력
 
-		long[] arr = new long[100];			// 100 크기의 arr 배열 // 숫자가 커서 long으로
+		if (N == 1) {
+			System.out.print("1");
+		} else if (N > 1 && N <= 100){
+			List<BigDecimal> arr = new ArrayList<BigDecimal>();			// List<BigDecimal> 형태로 변경
 
-		arr[0] = 1;							// 0번째 배열값 1 고정
-		arr[1] = 1;							// 1번째 배열값 1 고정
+			arr.add(0, new BigDecimal(1));							// 0번째 리스트값 1 고정
+			arr.add(1, new BigDecimal(1));						// 1번째 리스트값 1 고정
 
-		for (int i=2; i<100; i++){			// 2번째 배열부터 99번째 배열까지는 앞 두배열값 더한값
-			arr[i] = arr[i-1] + arr[i-2];				// 0부터 99배열까지 수 입력 완료
+			for (int i = 2; i < N; i++){			// 2번째 배열부터 N번째 . 앞 두배열값 더한값
+				BigDecimal y = arr.get(i-1);
+				BigDecimal z = arr.get(i-2);
+				BigDecimal x = y.add(z);
+				arr.add(x);							// 리스트에 x값(i번째까지) 추가
+			}
+			System.out.print(arr);
+		} else {
+			System.out.print("1부터 100까지의 자연수를 입력해주세요.");			// 예외처리
 		}
-
-		for (int i=0; i<N; i++) {			// i는 0번째부터 N-1배열, (N번째까지)
-			System.out.print(arr[i]+" ");
-		}
-
-		scan.close();						// 스캔 종료
+		scan.close();
 
 	}
-
 }
-
-// BigDecimal
